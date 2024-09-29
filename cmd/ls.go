d8916ab53186cefc695b7a66f40a83c047c9f2b1
edwarschaumanhaigh <eddjlsh@icloud.com>
2024-09-29 19:30:34 +0200

    Boilerplate

---
+++ cmd/ls.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	utils "github.com/Zoooooomies/go-ls/internal"
+)
+
+func main() {
+	fmt.Println(utils.Hello())
+}
---
+++ test/utils_test.go
@@ -0,0 +1,11 @@
+package utils
+
+import (
+	"testing"
+
+	utils "github.com/Zoooooomies/go-ls/internal"
+)
+
+func Test_Hello(t *testing.T) {
+	utils.Hello()
+}
